0cd5726296b7d4be8a035818471c281647f7e2f6
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-03-15 14:12:15 -0300

    feat: add illustration component

---
+++ components/Illustration.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const Illustration = () => {
+  return (
+    <Image
+      src="illustration-thank-you.svg"
+      alt="illustration thank you"
+      width={162}
+      height={108}
+    />
+  );
+};
+
+export default Illustration;
